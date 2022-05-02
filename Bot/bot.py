443046cc7085d93b68e0bc8aea0c49ee9988e3ff
Harshit-Kun <104750600+Harshit-Kun@users.noreply.github.com>
2022-05-02 11:05:43 +0530

    Create bot.py

---
+++ Bot/bot.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, filters
+
+bot = Client(
+    "my first project",
+    API_ID = ,
+    API_HASH = "",
+    BOT_TOKEN = ""
+)
+@bot.on_message(filters.command('start') & filters.private)
+ 
+def command1(bot, message):
+    bot.on_message(message.chat.id, "Heya , I am Nezuko ")
+
+bot.run()
