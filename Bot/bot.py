bf8583dda987f29b48641f4749df5086413f3b70
Harshit-Kun <104750600+Harshit-Kun@users.noreply.github.com>
2022-05-02 11:11:09 +0530

    Update bot.py

--- Bot/bot.py
+++ Bot/bot.py
@@ -2,9 +2,9 @@ from pyrogram import Client, filters
 
 bot = Client(
     "my first project",
-    API_ID = ,
-    API_HASH = "",
-    BOT_TOKEN = ""
+    API_ID = 8424747,
+    API_HASH = "8c1cf9dc795e9a2a69e80b068c8ad2a7",
+    BOT_TOKEN = "5046696779:AAEOB3ojcT7vc9HaX15YUWrhJNKXaXmIugM"
 )
 @bot.on_message(filters.command('start') & filters.private)
  
